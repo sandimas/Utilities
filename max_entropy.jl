4533d5c6422c162ab09acb9a0bd2d4b9f663ec3a
James Neuhaus <jneuhau1@vols.utk.edu>
2023-02-27 10:21:03 -0500

    cleaned up code

--- max_entropy.jl
+++ max_entropy.jl
@@ -1,25 +1,34 @@
+"""
+references:
+https://doi.org/10.1016/0370-1573(95)00074-7
+https://www.cond-mat.de/events/correl12/manuscripts/jarrell.pdf
+https://doi.org/10.1103/PhysRevE.94.023303
+"""
+
 using Statistics
 using LinearAlgebra
 using LogExpFunctions
 using LinearSolve
-using Dierckx
+using Dierckx # Spline functionality
 using Printf
 
 function calculate_spectral_function(G::AbstractMatrix{T},K::AbstractMatrix{T},m::AbstractVector{T};
-                                    als::AbstractVector{T} = exp10.(range(9, stop=1, length=1+20*(9-1)))) where {T}
-    #                                  ) where {T<:AbstractFloat}
+                                    als::AbstractVector{T} = exp10.(range(9, stop=1, length=1+20*(9-1)))) where {T<:AbstractFloat}
+    # Settings to ensure numerical stability
     W_ratio_max = 1e8
-    svd_threshold = 1e-12
-    n_bin = size(G,1)
-    # print(size(G),"x\n")
-    G_avg = Statistics.mean(G, dims=1)
+    svd_threshold = 1e-12 # drop very small singular values
     
+    # estimate multivariate Gaussian distro from G
+    n_bin = size(G,1)
+    G_avg = Statistics.mean(G, dims=1)
+
+    # calculate unitary matrix for diagonalizing covariance matrix
     svd_out = svd(G .- G_avg)
     sigma = svd_out.S
     Uc = svd_out.Vt
-    
     W=(n_bin*(n_bin-1))./(sigma .* sigma)
-    # print(size(W),W,"W\n")
+    
+    # deal with nearly singular covariance matrix by capping W
     W_cap = W_ratio_max * minimum(W)
     n_large = sum(maximum(W) > W_cap)
     if maximum(W) > W_cap
@@ -28,47 +37,43 @@ function calculate_spectral_function(G::AbstractMatrix{T},K::AbstractMatrix{T},m
                 W[i,1] = W_cap
             end
         end
-
     end
-    Kp = *(Uc,K)
-    # print(size(G_avg),G_avg,"gavg\n")
-    # print(size(Uc),Uc,"Uc\n")
-    
+
+    # rotate K and G_avg
+    Kp = Uc*K
     G_avg_p = zeros(T,n_bin)
-    # print("here\n")
     for i = 1:n_bin
-        # print(view(Uc,i,:),i,"Uc\n")
         G_avg_p[i] = dot(view(Uc,i,:),G_avg)
     end
+
+    # SVD of kernel
     svd_K = svd(Kp)
     V = svd_K.U
     Sigma = svd_K.S
     U = svd_K.Vt
-    # mask = Array{Bool}(n_bin)
+    
+    # drop singular values less than threshold
     mask = (Sigma/maximum(Sigma) .>= svd_threshold)
-    # print(size(mask),mask,"mask\n")
+
+    # precalculate some values reused in looped calc_A_al()
     SigmaVt = (V[:,mask] .* Sigma[mask])
-    # M_tmp = zeros(T,n_bin,n_bin)
-    # print(SigmaVt,"svt\n")
-    # print(size(SigmaVt),size(W),"size\n")
-    M_tmp = transpose(transpose(SigmaVt) .* W)
-    M =  *(M_tmp,transpose(SigmaVt))
+    M =  (W' .* SigmaVt)* SigmaVt'
     precalc = (U,SigmaVt,M)
-    # print(size(als,1),"als\n")
+
+    # arrays 
     us = zeros(size(als,1),size(M,1))
-    # print(size(us),size(us[1]),"us\n\n")
     chi2s = zeros(T,size(als))
     lnPs = zeros(T,size(als))
     dlnPs = zeros(T,size(als))
+    # loop over alphas with calc_A_al()
     for (i, al) in enumerate(als)
-        if i==1
-            _, us[i,:], chi2s[i], lnPs[i], dlnPs[i] = calc_A_al(G_avg_p, W, Kp, m, al, precalc, view(us,i,:))
-        else
-            _, us[i,:], chi2s[i], lnPs[i], dlnPs[i] = calc_A_al(G_avg_p, W, Kp, m, al, precalc, view(us,i,:))
-        end 
+        _, us[i,:], chi2s[i], lnPs[i], dlnPs[i] = calc_A_al(G_avg_p, W, Kp, m, al, precalc, view(us,i,:))
     end
+
+    # Sorted alphas needed for Spline function
     order = sortperm(als)
-    # BT
+
+    # Spline to fit, find max entropy
     fit = Spline1D(log.(als[order]),log.(chi2s[order]))
     k = derivative(fit,log.(als[order]),2)./(1 .+ derivative(fit,log.(als[order]),1).^2).^(1.5)
     max = findmax(k)[2]
@@ -77,33 +82,34 @@ function calculate_spectral_function(G::AbstractMatrix{T},K::AbstractMatrix{T},m
     chi2 = chi2s[order[max]]
     
     A = m .* exp.(*(U',us[max, :]))
-    print(A,"A\n\n")
+    print("\n", A,"\tA\n\n")
     dof = size(W,1) - n_large
-    err_str = @sprintf "alpha=%.3f\tchi2/dof=%.3f\tA.sum()=%.6f\n" al (chi2/dof) sum(A)
-    print(err_str)
-    # print(f"alpha={al:.3f}\tchi2/dof={chi2/dof:.3f}\tA.sum()={A.sum():6f}")
-
+    stat_str = @sprintf "alpha=%.3f\tchi2/dof=%.3f\tA.sum()=%.6f\n" al (chi2/dof) sum(A)
+    print(stat_str)
+  
     return A
 end
 
 function calc_A_al(G::AbstractArray{T}, W::AbstractArray{T}, K::AbstractArray{T}, m, al, precalc, u_init) where {T}
-    svd_threshold = 1e-12
-    mu_multiplier = 2.0
-    mu_min = al/4.0
-    mu_max = al*1e100
-    step_max_accept = 0.5
-    step_drop_mu = 0.125
+    mu_multiplier = 2.0 # Increase/decrease mu by this factor 
+    mu_min = al/4.0 # minimum for non-zero mu
+    mu_max = al*1e100 # maximum for non-zero mu
+    step_max_accept = 0.5 # maximum size of accepted step
+    step_drop_mu = 0.125 # decrease mu if step_size < this
     dQ_threshold = 1e-10
-    max_small_dQ = 7
-    max_iter = 1234
+    max_small_dQ = 7 # stop if dQ/Q < dQ_threshold this many times in a row
+    max_iter = 1234 # maximum number of iterations if conditions not met
+
     Q_new = 0
+    n_bin = size(G,1)
+    n_omega = size(m,1)
+     
+    # initial state
     U, SigmaVt, M = precalc
     u = u_init
     mu=al
-    n_bin = size(G,1)
-    n_omega = size(m,1)
     Id = 1.0*Matrix(I,size(M))
-    # print(size(U),size(u),"U\n")
+   
     pre_exp = zeros(size(m))
     for i = 1:size(m,1)
         pre_exp[i] = dot(u,view(U,:,i))
@@ -112,22 +118,21 @@ function calc_A_al(G::AbstractArray{T}, W::AbstractArray{T}, K::AbstractArray{T}
     for i = 1:size(m,1)
         A[i] = m[i] * exp(pre_exp[i])
     end
-    f = al*u + *(SigmaVt,(*(K,A)-G) .* W)
-    # print(size(A),size(U),"sizes\n")
-    Tmat =  *(transpose(transpose(U) .* A),transpose(U))
-    MT = *(M,Tmat)
-    # print(size(MT),MT,"MT\n")
+    f = al*u + SigmaVt*((*(K,A)-G) .* W)
+    Tmat = (U*(U' .* A))
+    MT = Tmat*M'
+    
     Q_old, S, chi2 = _Q(A, G, W, K, m, al)
+
+    # search for max entryop
     small_dQ = 0
     for i=1:max_iter
         prob = LinearProblem((al+mu)*Id+MT,-f)
-        du = transpose(solve(prob).u)
-        # print(du,"du\n")
-        step_size = dot(*(du,Tmat),du)
-        # print(step_size,"step\n")
-        # print(size(u),size(du),size(U))
+        du = (solve(prob).u)'
+    
+        step_size = (du*Tmat)⋅du
         for i = 1:size(m,1)
-            pre_exp[i] = dot(view(U,:,i),u + transpose(du))
+            pre_exp[i] = dot(view(U,:,i),u + du')
         end
         for i = 1:size(m,1)
             A[i] = m[i] * exp(pre_exp[i])
@@ -135,7 +140,7 @@ function calc_A_al(G::AbstractArray{T}, W::AbstractArray{T}, K::AbstractArray{T}
         Q_new, S, chi2 = _Q(A, G, W, K, m, al)
         Q_ratio = Q_new/Q_old
         if step_size < step_max_accept && Q_ratio < 1000
-            u += transpose(du)
+            u += du'
             if abs(Q_ratio) < dQ_threshold
                 small_dQ += 1
                 if small_dQ == max_small_dQ
@@ -152,13 +157,14 @@ function calc_A_al(G::AbstractArray{T}, W::AbstractArray{T}, K::AbstractArray{T}
                 end
             end
             f = al*u + *(SigmaVt,(*(K,A)-G) .* W)
-            Tmat =  *(transpose(transpose(U) .* A),transpose(U))
+            Tmat =  (U*(U' .* A))'
             MT = *(M,Tmat)
             Q_old = Q_new
         else
             mu = clamp(mu*mu_multiplier,mu_min,mu_max)
         end
     end
+    # calculate Z ∝ ln P(α|G,m)
     Z = zeros(T,n_bin,n_omega)
     for i =1:n_bin
         Z[i,1:4] = view(sqrt.(W)[:] .* K,i,:) .* sqrt.(A)
@@ -172,9 +178,7 @@ function calc_A_al(G::AbstractArray{T}, W::AbstractArray{T}, K::AbstractArray{T}
     end
     S = sum(S_vec)
     dlnP = sum(lam./(al .+ lam))/(2 * al) + S 
-    # print(dlnP,"dlnp\n")
-    # print(u,"u\n")
-    # exit()
+    
     return A, u, chi2, lnP, dlnP
 end
 
@@ -189,7 +193,7 @@ function _Q(A, G, W, K, m, al)
     return al*S - 0.5*chi2, S, chi2
 end
 
-
+# Fermion kernel
 function kernel_f(β::Real,τ::AbstractVector{T},ω::AbstractVector{T}) where {T}
     K = zeros(T,length(τ),length(ω))
     for oi = 1:length(ω)
@@ -201,6 +205,22 @@ function kernel_f(β::Real,τ::AbstractVector{T},ω::AbstractVector{T}) where {T
     return K
 end
 
+function kernel_b(β::Real,τ::AbstractVector{T},ω::AbstractVector{T},sym=true) where {T}
+    K = zeros(T,length(τ),length(ω))
+    for oi = 1:length(ω)
+        denom = 1.0 - exp(-β*ω[oi])
+        if sym
+            for ti = 1:length(τ)
+                K[ti,oi] = ω* (exp(-τ[ti]*ω[oi]) + exp(-(β- τ[ti])*ω[oi]))/denom
+            end
+        else
+            for ti = 1:length(τ)
+                K[ti,oi] = ω* (exp(-τ[ti]*ω[oi]))/denom
+            end
+        end
+    end
+    return K
+end
 
 model = [0.1,.5,.3,.1]
 beta = 1.0
